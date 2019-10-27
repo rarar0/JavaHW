4f219d9c11e628d1e731246e73dc3fdbeaa1c08f
rarar0/0 <boombot101@gmail.com>
2019-10-27 12:58:07 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,1 +1,29 @@
-
+public class Circle
+{
+    double radius;
+    public Circle(double r)
+        {
+        radius = r;
+        }
+    public void setRadius(double r)
+    {
+       radius = r;
+    }
+    public double getRadius()
+    {
+        return radius;
+    }
+    public double area()
+    {
+        return Math.PI * Math.pow(radius, 2);
+    }
+    public double diameter()
+    {
+        return radius * 2.0;
+    }
+    public double circumference()
+    {
+        return Math.PI * radius * 2.0;
+    }
+}
+    
