ecf6deb398900f80c6f3718e78fbfbae7ac236e0
rarar0/0 <boombot101@gmail.com>
2019-11-05 13:03:18 -0600

    Update TelevisionDemo.java

--- TelevisionDemo.java
+++ TelevisionDemo.java
@@ -56,12 +56,37 @@ public class TelevisionDemo
       // Display the the current channel and
       // volume of the television
       System.out.println("Channel: " +
-                         bigScreen.getChannel() +
-                         " Volume: " +
-                         bigScreen.getVolume());
+      bigScreen.getChannel() +
+      " Volume: " +
+      bigScreen.getVolume());
 
-      System.out.println();   // For a blank line
+      System.out.println();  // For a blank line
 
-      // HERE IS WHERE YOU DO TASK #5
+      //  HERE IS WHERE YOU DO TASK #5
+      Television portable = new Television("Sharp", 19);
+      //  Turn the power on
+      portable.power();
+      //  Display the state of the television
+      System.out.println("A " + portable.getScreenSize() + " inch " +
+                         portable.getManufacturer()	
+                         + " has been turned on.");
+      //  Prompt the user for input and store into station
+      System.out.print("What channel do you want?  ");
+      station = keyboard.nextInt();
+      //  Change the channel on the television
+      portable.setChannel(station);
+      //  Increase the volume of the television
+      portable.increaseVolume();
+      //  Display the the current channel and volume of the television
+      System.out.println("Channel:  " + portable.getChannel() +
+                         " Volume: "+ portable.getVolume());
+      System.out.println("Too loud!! Lowering the volume.");
+      //  Decrease the volume of the television
+      portable.decreaseVolume();
+      portable.decreaseVolume();
+      portable.decreaseVolume();
+      //  Display the current channel and volume of the television
+      System.out.println("Channel:  "+ portable.getChannel() +
+                         " Volume: "+ portable.getVolume());
    }
 }
