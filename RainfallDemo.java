22ecc44fa57297d0b53936ea523c3ad7b4f611b0
rarar0/0 <boombot101@gmail.com>
2019-11-05 20:02:53 -0600

    Create RainfallDemo.java

---
+++ RainfallDemo.java
@@ -0,0 +1,14 @@
+/**
+   This program demonstrates a solution to the
+   Rainfall Class programming challenge.
+*/
+
+public class RainfallDemo
+{
+    public static void main(String[] args)
+    {
+       // Create an array of rainfall figures.
+       double[] thisYear = {1.6, 2.1, 1.7, 3.5, 2.6, 3.7,
+                            3.9, 2.6, 2.9, 4.3, 2.4, 3.7 };
+    }
+}
