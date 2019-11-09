60a838499f43454446ce6de35773de31a7d38989
rarar0/0 <boombot101@gmail.com>
2019-11-09 16:50:26 -0600

    Update GradeBookDemo.java

--- GradeBookDemo.java
+++ GradeBookDemo.java
@@ -1,1 +1,31 @@
+import java.io.*;
+import java.util.*;
 
+public class GradeBookDemo
+{
+    public static void main(String[] args) throws IOException
+    {
+        String filename = "StudentInfo.txt";
+        FileReader File = new FileReader(filename); 
+        BufferedReader readFile = new BufferedReader(File);
+        String line = readFile.readLine();      
+        String[] test = new String[25];
+        while (true)
+        {
+            line = readFile.readLine(); // Read a new line from the file
+    	    if(line == null)
+    		    break;
+    	    else
+            {
+                for (int i = 0; i < test.length; i++)
+                {
+                    test[i] = readFile.readLine();
+                }
+            }
+        }
+        for (int i = 0; i <= 25; i++)
+        {
+            System.out.println(test[i]);
+        }
+    }
+}
