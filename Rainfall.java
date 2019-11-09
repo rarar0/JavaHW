53e5adf49dc84f598ad6a2b9906baf334858aada
rarar0/0 <boombot101@gmail.com>
2019-11-09 13:34:41 -0600

    Update Rainfall.java

--- Rainfall.java
+++ Rainfall.java
@@ -1,9 +1,13 @@
 public class Rainfall
 {
     private double[] rain = new double[12];
+    private String[] monthNames = {"January", "February", "March", 
+                                   "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"};
+
     public Rainfall(double[] r)
     {
-        rain = r;
+        
     }
     public double getTotalRainFall()
     {
@@ -16,25 +20,38 @@ public class Rainfall
     }
     public double getAverageRainFall()
     {
-        return Rainfall.getTotalRainFall() / 12;
+        int total = 0;
+        for (int i=0; i < rain.length; i++)
+        {
+            total += rain[i];
+        }
+        return total / 12.0;
     }
     public int getHighestMonth()
     {
         double highest = rain[0];
+        int highestIterator = 0;
+        
         for (int i=1; i < rain.length; i++)
         {
             if (rain[i] > highest)
                 highest = rain[i];
+                highestIterator = i
         }
+        return monthNames[highestIterator];
     }
     public int getLowestMonth()
     {
         double lowest = rain[0];
+        int lowestIterator = 0;
         for (int i=1; i < rain.length; i++)
         {
             if (rain[i] < lowest)
                 lowest = rain[i];
+                lowestIterator = i
+                
         }
+        return monthNames[lowestIterator];
     }
     public double getRainAt(int e)
     {
