2cbf4ed160f22120086a210b99d1b6c4dc2971f9
rarar0/0 <boombot101@gmail.com>
2019-11-12 14:02:44 -0600

    Create GradeBook.java

---
+++ GradeBook.java
@@ -0,0 +1,49 @@
+public class GradeBook
+{
+    final private int NUM_STUDENTS = 5;
+    final private int NUM_TESTS = 4;
+    private String[] names = new String[NUM_STUDENTS];
+    private char[] grades = new char[NUM_TESTS];
+    private double[] scores1 = new double[NUM_TESTS];
+    private double[] scores2 = new double[NUM_TESTS];
+    private double[] scores3 = new double[NUM_TESTS];
+    private double[] scores4 = new double[NUM_TESTS];
+    private double[] scores5 = new double[NUM_TESTS];
+    
+    public void setName(int StudentNum, String name)
+    {
+        
+    }
+    public void setScores(int studentNum, double[] scores)
+    {
+        
+    }
+    public String getName(int studentNum)
+    {
+        return names[studentNum];
+    }
+    public double getAverage(int studentNum)
+    {
+        
+    }
+    public char getLetterGrade(int studentNum)
+    {
+        
+    }
+    private void copyArray(double[] to, double[] from)
+    {
+        
+    }
+    private double calcAverage(double[] scores)
+    {
+        
+    }
+    private void assignGrade(int studentNum)
+    {
+        
+    }
+    private char determineGrade(double average)
+    {
+        
+    }
+}
