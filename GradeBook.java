fa84966d942395cd5cc3ff917dc18beebd93ae43
rarar0/0 <boombot101@gmail.com>
2019-11-12 17:44:27 -0600

    Update GradeBook.java

--- GradeBook.java
+++ GradeBook.java
@@ -10,13 +10,22 @@ public class GradeBook
     private double[] scores4 = new double[NUM_TESTS];
     private double[] scores5 = new double[NUM_TESTS];
     
-    public void setName(int StudentNum, String name)
+    public void setName(int studentNum, String name)
     {
-        
+        names[studentNum] = name;
     }
     public void setScores(int studentNum, double[] scores)
     {
-        
+        if (studentNum == 1)
+            copyArray(scores1, scores);
+        else if (studentNum == 2)
+            copyArray(scores2, scores);
+        else if (studentNum == 3)
+            copyArray(scores3, scores);
+        else if (studentNum == 4)
+            copyArray(scores4, scores);
+        else if (studentNum == 5)
+            copyArray(scores5, scores);
     }
     public String getName(int studentNum)
     {
@@ -28,19 +37,36 @@ public class GradeBook
     }
     public char getLetterGrade(int studentNum)
     {
-        
+        return grades[studentNum];
     }
     private void copyArray(double[] to, double[] from)
     {
-        
+        for (int i = 0; i <to.length; i++)
+        {
+            to[i] = from[i];
+        }
     }
     private double calcAverage(double[] scores)
     {
-        
+        double total = 0.0, average, lowest;
+        for (int i=0; i < scores.length; i++)
+        {
+            total += scores[i];
+        }
+        return total / scores.length;
     }
     private void assignGrade(int studentNum)
     {
-        
+        if (studentNum == 1)
+            determineGrade();
+        else if (studentNum == 2)
+            
+        else if (studentNum == 3)
+            
+        else if (studentNum == 4)
+            
+        else if (studentNum == 5)
+            
     }
     private char determineGrade(double average)
     {
