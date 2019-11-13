b0dfa7c0abcc9d1e75ae0f0384126a92f1333d10
rarar0/0 <boombot101@gmail.com>
2019-11-12 21:03:48 -0600

    Update PhoneBookEntry.java

--- PhoneBookEntry.java
+++ PhoneBookEntry.java
@@ -1,1 +1,27 @@
-
+public class PhoneBookEntry
+{
+    private String name;
+    private String phoneNumber;
+    
+    public PhoneBookEntry(String n, String pn)
+    {
+        name= n;
+        phoneNumber = pn;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+    public void setName(String n)
+    {
+        name = n;
+    }
+    public void setPhoneNumber(String pn)
+    {
+        phoneNumber = pn;
+    }
+}
