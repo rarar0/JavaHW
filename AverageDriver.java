5593f08e6f0ef27357da84cbe5a709a8b161ccb5
rarar0/0 <boombot101@gmail.com>
2019-11-19 16:29:17 -0600

    Update AverageDriver.java

--- AverageDriver.java
+++ AverageDriver.java
@@ -1,1 +1,14 @@
+import java.util.*;
+public class AverageDriver
+{
+    public static void main(String[] args)
+    {
+        Average avg = new Average();
+        printString(avg);
+    }
+    public static void printString(Average avg)
+        {
+            System.out.println(avg.toString());
 
+        }
+}
