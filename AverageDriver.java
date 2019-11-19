9489dd7282425e3aaf2da4dd715ddd798bf45eea
rarar0/0 <boombot101@gmail.com>
2019-11-19 17:12:04 -0600

    Update AverageDriver.java

--- AverageDriver.java
+++ AverageDriver.java
@@ -1,4 +1,8 @@
 import java.util.*;
+/**
+AverageDriver class creates an Average object
+and then calls the printString method
+*/
 public class AverageDriver
 {
     public static void main(String[] args)
@@ -6,6 +10,10 @@ public class AverageDriver
         Average avg = new Average();
         printString(avg);
     }
+    /**
+    The printString method calls the Average
+    object and the toString method, and prints
+    */
     public static void printString(Average avg)
         {
             System.out.println(avg.toString());
