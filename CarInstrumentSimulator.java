afe352ff51d9eda3c000832049992d51a1542169
rarar0/0 <boombot101@gmail.com>
2019-11-26 13:54:59 -0600

    Update CarInstrumentSimulator.java

--- CarInstrumentSimulator.java
+++ CarInstrumentSimulator.java
@@ -1,28 +1,33 @@
+/**
+CarInstrumentSimulator utilizes the FuelGauge and Odometer classes
+to print out a simulated car ride.
+*/
 public class CarInstrumentSimulator
 {
 
     public static void main(String[] args) 
     {
 
+        // Creates a FuelGauge object
         FuelGauge fuel = new FuelGauge();
+        // Creates an Odometer object
         Odometer odometer = new Odometer(0, fuel);
-
-        for (int x = 0; x < FuelGauge.MAXIMUM_GALLONS; x++) 
+        
+        // Executes a loop to fill the gastank
+        for (int i = 0; i < FuelGauge.MAXIMUM_GALLONS; i++) 
         {
-            fuel.addGallons();
+            fuel.incrementGallons();
         }
-
+        // Executes a loop while the gastank has gas
         while (fuel.getGallons() > 0) 
         {
-
             // mile driven
-            odometer.addMileage();
-
-            // the mileage.
+            odometer.incrementMileage();
+            // mileage printout
             System.out.println("Mileage: " + odometer.getMileage());
-
-            // the amount of fuel.
+            // fuel printout
             System.out.println("Fuel level: " + fuel.getGallons() + " gallons");
+            // newline
             System.out.println("--------------------------------------");
         }
     }
