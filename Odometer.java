8f22764ff7dbaf08bb6d4ff300aecbac2f3c30ff
rarar0/0 <boombot101@gmail.com>
2019-11-26 13:55:06 -0600

    Update Odometer.java

--- Odometer.java
+++ Odometer.java
@@ -1,55 +1,57 @@
-class Odometer {
+class Odometer 
+{
 
     // Constant for the maximum mileage
     public final int MAXIMUM_MILEAGE = 999999;
-
     // Constant for the miles-per-gallon
     public final int MPG = 24;
-
-    private int initialMileage;
     private int mileage;
-
+    private int setPoint;
     // Field to reference a FuelGauge object
     private FuelGauge fuelGauge;
-
     /**
-     * Constructor
-     *
-     * @param m
-     *            Initial mileage.
-     * @param fg
-     *            A reference to a FuelGauge object.
-     */
-    public Odometer(int mileage, FuelGauge fuelGauge) {
-        this.initialMileage = mileage;
+    Odometer constructor
+    @param mileage sets initial mileage.
+    @param fuelGauge references FuelGauge object.
+    */
+    public Odometer(int mileage, FuelGauge fuelGauge) 
+    {
+        this.setPoint = mileage;
         this.mileage = mileage;
         this.fuelGauge = fuelGauge;
     }
 
     /**
-     * getMileage method
-     *
-     * @returns The mileage.
-     */
-    public int getMileage() {
+    The getMileage method
+    @return The mileage.
+    */
+    public int getMileage() 
+    {
         return mileage;
     }
 
     /**
-     * The addMileage method increments the mileage field. If mileage
-     * exceeds the maximum amount, it rolls over to 0.
-     */
-    public void addMileage() {
+    The incrementMileage method increments the mileage and
+    checks MPG. If mileage is greater than max, it resets to 0.
+    */
+    public void incrementMileage() 
+    {
 
-        if (mileage < MAXIMUM_MILEAGE) {
+        if (mileage < MAXIMUM_MILEAGE) 
+        {
             mileage++;
-        } else {
+        } 
+        else 
+        {
             mileage = 0;
         }
-
-        int driven = initialMileage - mileage;
-        if (driven % MPG == 0) {
-            fuelGauge.burnFuel();
+        // calls fuelGauge to decrementGallons every time a gallons worth
+        // of driving occurs
+        int milesDriven = setPoint - mileage;
+        // Every 24 miles, fuelGauge.decrementGallons executes
+        if (milesDriven % MPG == 0) 
+        {
+            fuelGauge.decrementGallons();
         }
     }
 }
